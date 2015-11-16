2a90290a2add41233576efa58830a565053941e8
Lance Woodson <lance.woodson@bazaarvoice.com>
2015-11-15 20:45:29 -0600

    Get bundy executable working with cli-specified options

--- src/com.devquixote.bundy/api/api.go
+++ src/com.devquixote.bundy/api/api.go
@@ -1,20 +1,36 @@
 package api
 
 import (
+    "fmt"
     "io"
     "net/http"
+    "syscall"
+
+    "com.devquixote.bundy/opts"
 )
 
 type Api struct {
-    Address string
-    Path string
+    Options opts.Options
 }
 
 func (api *Api) HandleRequest(writer http.ResponseWriter, response *http.Request) {
-    io.WriteString(writer, "Hello, World!")
+    for _, pid := range api.Options.Pids {
+        for _, signal := range api.Options.ConvertedSignals() {
+            if err := syscall.Kill(pid, signal); err == nil {
+                fmt.Printf("Process %v %v\n", pid, signal)
+                break
+            }
+        }
+
+    }
+    io.WriteString(writer, "Bundy has killed again!\n")
+    if api.Options.Suicide {
+        go syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+    }
 }
 
 func (api *Api) Start() {
-    http.HandleFunc(api.Path, api.HandleRequest)
-    http.ListenAndServe(api.Address, nil)
+    fmt.Printf("Starting bundy server (%v)\n", api.Options.AddressAndPort())
+    http.HandleFunc("/kill", api.HandleRequest)
+    http.ListenAndServe(api.Options.AddressAndPort(), nil)
 }
--- src/com.devquixote.bundy/bundy.go
+++ src/com.devquixote.bundy/bundy.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-    "fmt"
+    "os"
 
+    "github.com/jessevdk/go-flags"
+
+    "com.devquixote.bundy/opts"
     "com.devquixote.bundy/api"
 )
 
 func main() {
-    fmt.Printf("Starting bundy server\n")
-    api := api.Api{"127.0.0.1:6666", "/kill"}
+    var options opts.Options
+    if _, err := flags.ParseArgs(&options, os.Args); err != nil {
+        os.Exit(1)
+    }
+    // api := api.Api{"127.0.0.1:6666", "/kill"}
+    api := api.Api{options}
     api.Start()
 }
